66677507cfcd9904234ad9845a1246d6e79a531a
Owen Siebers <RobotLeopard86>
2020-09-28 14:00:42 -0700

    Download bug should now be fixed

--- lc-versionscript.js
+++ lc-versionscript.js
@@ -4,17 +4,15 @@ let v0_2_0$1_15_2 = "downloads/1.15.2/leopardcraft/leopardcraft-0.2.0.zip";
 let mcversions = document.getElementById("mcversions");
 let lcversions1_15_2 = document.getElementById("1-15-2v");
 
-let urlbegin = "https://robotleopard86.github.io/ModdingHub/"
-
 function download(){
     if(confirm("Are you sure you want to download LeopardCraft?")){
         var version = lcversions1_15_2.options[lcversions1_15_2.selectedIndex].value;
         switch(version){
             case "0.1.0/1.15.2":
-                download(urlbegin + v0_1_0$1_15_2);
+                document.createElement("a", "href = '" + v0_1_0$1_15_2 + "' download", "Download Now");
                 break;
             case "0.2.0/1.15.2":
-                download(urlbegin + v0_2_0$1_15_2);
+                document.createElement("a", "href = '" + v0_2_0$1_15_2 + "' download", "Download Now");
                 break;
             default:
                 break;
